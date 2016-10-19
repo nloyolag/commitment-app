97155bd1699ddd57d5e3924afb60df7476db66c7
Noe Loyola <noeloyolag@gmail.com>
2016-10-19 17:25:58 -0500

    Initial commit: Project Structure

---
+++ app/src/main/java/itesm/mx/commitment_app/Commitments.java
@@ -0,0 +1,13 @@
+package itesm.mx.commitment_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Commitments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commitments);
+    }
+}
---
+++ app/src/main/java/itesm/mx/commitment_app/Surveys.java
@@ -0,0 +1,13 @@
+package itesm.mx.commitment_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Surveys extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_surveys);
+    }
+}
