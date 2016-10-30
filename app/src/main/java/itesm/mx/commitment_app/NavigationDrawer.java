631c4bd697e75ccb1f881953439744a2b9baaef1
Noe Loyola <noeloyolag@gmail.com>
2016-10-29 21:09:02 -0500

    add: Logout

--- app/src/main/java/itesm/mx/commitment_app/NavigationDrawer.java
+++ app/src/main/java/itesm/mx/commitment_app/NavigationDrawer.java
@@ -1,5 +1,6 @@
 package itesm.mx.commitment_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class NavigationDrawer extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -92,6 +95,10 @@ public class NavigationDrawer extends AppCompatActivity
 
         } else if (id == R.id.nav_send) {
 
+        } else if (id == R.id.nav_logout) {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(NavigationDrawer.this, Login.class));
+            finish();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
