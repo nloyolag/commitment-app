4cc417e36e0e3b7702465cdfa8d3f1933a03e64c
Noe Loyola <noeloyolag@gmail.com>
2016-11-06 11:40:57 -0600

    add: Some stuff to gradle files

--- app/src/main/java/itesm/mx/commitment_app/CreateAccount.java
+++ app/src/main/java/itesm/mx/commitment_app/CreateAccount.java
@@ -17,6 +17,8 @@ import java.util.regex.Pattern;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CreateAccount extends AppCompatActivity {
 
