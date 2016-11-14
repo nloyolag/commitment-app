7e6a0e6c4a04ec9d9291553128f4abe49dfc1701
Noe Loyola <noeloyolag@gmail.com>
2016-11-14 17:38:03 -0600

    add: List of projects in user home

--- app/src/main/java/itesm/mx/commitment_app/UserHomeGrid.java
+++ app/src/main/java/itesm/mx/commitment_app/UserHomeGrid.java
@@ -9,20 +9,24 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 /**
  * Created by luis on 10/11/16.
  */
 
 class UserHomeGrid extends ArrayAdapter<String> {
     private final Activity context;
-    private final String [] projectname;
-    private int [] imageId;
+    private final ArrayList<String> project_names;
+    private final ArrayList<String> project_ids;
+    private ArrayList<Integer> image_ids;
 
-    UserHomeGrid(Activity context, String [] projectname, int [] imageId) {
-        super(context, R.layout.user_home_grid_adapter, projectname);
+    UserHomeGrid(Activity context, ArrayList<String> project_names, ArrayList<String> project_ids, ArrayList<Integer> image_ids) {
+        super(context, R.layout.user_home_grid_adapter, project_names);
         this.context = context;
-        this.projectname = projectname;
-        this.imageId = imageId;
+        this.project_names = project_names;
+        this.project_ids = project_ids;
+        this.image_ids = image_ids;
     }
 
     public View getView(int position, View view, @NonNull ViewGroup parent){
@@ -30,10 +34,14 @@ class UserHomeGrid extends ArrayAdapter<String> {
         View rowView= inflater.inflate(R.layout.user_home_grid_adapter, null, true);
 
         TextView txtView = (TextView) rowView.findViewById(R.id.textView_user_home);
-        txtView.setText(projectname[position]);
+        txtView.setText(project_names.get(position));
+
+        TextView idView = (TextView) rowView.findViewById(R.id.textView_id);
+        idView.setVisibility(View.GONE);
+        idView.setText(project_ids.get(position));
 
         ImageView imageView = (ImageView) rowView.findViewById(R.id.imageView_user_home);
-        imageView.setImageResource(imageId[position]);
+        imageView.setImageResource(image_ids.get(position));
 
         return rowView;
     }
